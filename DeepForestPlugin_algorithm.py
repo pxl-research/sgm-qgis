7e7597940c9998a739ddb3f2a8ff1cfa77660e9e
Servaas Tilkin <31237936+stilkin-pxl@users.noreply.github.com>
2023-05-09 15:22:32 +0200

    Added extra input parameters

--- DeepForestPlugin_algorithm.py
+++ DeepForestPlugin_algorithm.py
@@ -60,7 +60,10 @@ class DeepForestPluginAlgorithm(QgsProcessingAlgorithm):
 
     OUTPUT = 'OUTPUT'
     INPUT = 'INPUT'
-    INPUT_LIMIT = 'INPUT_LIMIT'
+    INPUT_TILE = 'INPUT_TILE'
+    INPUT_WINDOW = 'INPUT_WINDOW'
+    INPUT_THRESH = 'INPUT_THRESH'
+    INPUT_IOU_THRESH = 'INPUT_IOU_THRESH'
 
     MSG_SRC = 'DeepForestPluginAlgorithm'
     MSG_INFO = 0
@@ -76,23 +79,74 @@ class DeepForestPluginAlgorithm(QgsProcessingAlgorithm):
             QgsProcessingParameterRasterLayer(
                 self.INPUT,
                 self.tr('Input layer'),
-                None,
-                False
+                optional=False
             )
         )
 
-        # Add window parameter for algorithm
+        # Add Tile slicing parameter for algorithm
         self.addParameter(
             QgsProcessingParameterNumber(
-                self.INPUT_LIMIT,
-                self.tr('Window size in pixels (should be about 40m)'),
+                self.INPUT_TILE,
+                self.tr('Tile slicing size'),
                 type=QgsProcessingParameterNumber.Integer,
-                defaultValue=600,
-                optional=False,
-                minValue=0,
-                maxValue=10000,
+                defaultValue=3500,
+                optional=True,
+                minValue=2000,
+                maxValue=8000,
             )
         )
+        self.parameterDefinition(self.INPUT_TILE).setHelp(
+            'The map is cut into slices before being processed by the tree detector. ' +
+            'Smaller tiles process more quickly but have more problems at the edges. ' +
+            'Defaults to 3500')
+
+        # Add Patch size parameter for algorithm
+        self.addParameter(
+            QgsProcessingParameterNumber(
+                self.INPUT_WINDOW,
+                self.tr('Patch size in pixels'),
+                type=QgsProcessingParameterNumber.Integer,
+                defaultValue=900,
+                optional=True,
+                minValue=100,
+                maxValue=3000,
+            )
+        )
+        self.parameterDefinition(self.INPUT_WINDOW).setHelp(
+            'The algorithm scans for trees in a window of this size. ' +
+            'It should be about equivalent to 40m in the real world. ' +
+            'Defaults to 900')
+
+        # Add threshold parameter for algorithm
+        self.addParameter(
+            QgsProcessingParameterNumber(
+                self.INPUT_THRESH,
+                self.tr('Tree detection threshold (default 0.5)'),
+                type=QgsProcessingParameterNumber.Double,
+                defaultValue=0.5,
+                optional=True,
+                minValue=0.05,
+                maxValue=0.95,
+            )
+        )
+        self.parameterDefinition(self.INPUT_THRESH).setHelp(
+            'Below this value, a detected object will not be classified as a tree. Defaults to 0.5')
+
+        # Add overlap threshold parameter for algorithm
+        self.addParameter(
+            QgsProcessingParameterNumber(
+                self.INPUT_IOU_THRESH,
+                self.tr('Overlap detection threshold'),
+                type=QgsProcessingParameterNumber.Double,
+                defaultValue=0.5,
+                optional=True,
+                minValue=0.05,
+                maxValue=0.95,
+            )
+        )
+        self.parameterDefinition(self.INPUT_IOU_THRESH).setHelp(
+            'Minimum iou overlap among predictions between windows to be suppressed. Defaults to 0.5.' +
+            'Lower values suppress more boxes at edges.')
 
         # Output parameter is a folder on the users computer
         self.addParameter(
@@ -104,6 +158,9 @@ class DeepForestPluginAlgorithm(QgsProcessingAlgorithm):
                 # fileFilter='GeoJSON files (*.json)'
             )
         )
+        self.parameterDefinition(self.OUTPUT).setHelp(
+            'The generated GeoJSON will be placed in this folder after processing' +
+            'Make sure this folder is writable.')
 
     # https://github.com/geoscan/geoscan_forest
     # https://bitbucket.org/kul-reseco/localmaxfilter/src/master/localmaxfilter/interfaces/localmaxfilter_processing.py
@@ -199,6 +256,15 @@ class DeepForestPluginAlgorithm(QgsProcessingAlgorithm):
                             ymax = 1 - (y0 + json_boxes[b]['ymax']) / sl_height
                             ymax = sl_rect.yMinimum() + (ymax * sl_rect.height())
 
+                            properties = {
+                                'stroke': '#00ff00',
+                                'stroke-width': 1,
+                                'fill': '#00ff00',
+                                'fill-opacity': 0.5
+                            }
+
+                            properties.update(json_boxes[b])
+
                             feature = {
                                 "type": "Feature",
                                 "geometry": {
@@ -211,7 +277,7 @@ class DeepForestPluginAlgorithm(QgsProcessingAlgorithm):
                                         [xmin, ymin]
                                     ]]
                                 },
-                                "properties": json_boxes[b]
+                                "properties": properties
                             }
                             feature_list.append(feature)
                     else:
@@ -230,9 +296,9 @@ class DeepForestPluginAlgorithm(QgsProcessingAlgorithm):
                 'type': 'FeatureCollection',
                 'features': feature_list,
                 'crs': {
-                    "type": "name",
-                    "properties": {
-                        "name": crs
+                    'type': 'name',
+                    'properties': {
+                        'name': crs
                     }
                 }
             }
